ec374463a8739388dd31af1052f041191e67685d
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2023-12-13 22:49:30 +0530

    Added Enum for Hard Constraints

--- constraints.py
+++ constraints.py
@@ -1,70 +1,145 @@
-from enum import Enum, auto
+from enum import Enum
+from typing import Tuple, Dict
 
-# Option 1
 class Parameter(Enum):
-    rLE = auto()       # Leading edge radius
-    Xup = auto()       # Upper crest abscissa
-    Yup = auto()       # Upper crest ordinate
-    YXXup = auto()     # Upper crest curvature
-    Xlow = auto()      # Lower crest abscissa
-    Ylow = auto()      # Lower crest ordinate
-    YXXlow = auto()    # Lower crest curvature
-    yTE = auto()       # Trailing edge ordinate
-    deltaYTE = auto()  # Trailing edge thickness
-    alphaTE = auto()   # Trailing edge direction angle
-    betaTE = auto()    # Trailing edge wedge angle
+    rLE = ('rLE', (0.0085, 0.0126))
+    Xup = ('Xup', (0.41, 0.46))
+    Yup = ('Yup', (0.11, 0.13))
+    YXXup = ('YXXup', (-0.9, -0.7))
+    Xlow = ('Xlow', (0.20, 0.26))
+    Ylow = ('Ylow', (-0.023, -0.015))
+    YXXlow = ('YXXlow', (0.05, 0.20))
+    yTE = ('yTE', (-0.006, -0.003))
+    deltaYTE = ('deltaYTE', (0.0025, 0.0050))
+    alphaTE = ('alphaTE', (7.0, 10.0))
+    betaTE = ('betaTE', (10.0, 14.0))
 
-class Constraint(Enum):
-    CHORD_LENGTH = 1.0
-    MIN_CURVATURE = -0.1
-    MAX_CURVATURE = 0.1
-    MAX_THICKNESS = 0.1
+    def __init__(self, param_name: str, valid_range: Tuple[float, float]):
+        self.param_name = param_name
+        self.valid_range = valid_range
 
+def get_valid_range(parameter: Parameter) -> Tuple[float, float]:
+    """
+    Get the valid range for a given parameter.
 
-# Option 2 - with Flexibility
-class Parameter(Enum):
-    def __new__(cls, min_value, max_value, soft_min=None, soft_max=None):
-        obj = object.__new__(cls)
-        obj._value_ = obj  # Unique value for each member
-        obj.min_value = min_value
-        obj.max_value = max_value
-        obj.soft_min = soft_min if soft_min is not None else min_value
-        obj.soft_max = soft_max if soft_max is not None else max_value
-        return obj
+    Args:
+    - parameter (Parameter): The enum member representing the parameter.
 
-    rLE = (0.005, 0.05)       # Leading edge radius
-    Xup = (0, 1.0)            # Upper crest abscissa
-    Yup = (-0.2, 0.2)         # Upper crest ordinate
-    YXXup = (-0.1, 0.1)       # Upper crest curvature
-    Xlow = (0, 1.0)           # Lower crest abscissa
-    Ylow = (-0.2, 0.2)        # Lower crest ordinate
-    YXXlow = (-0.1, 0.1)      # Lower crest curvature
-    yTE = (-0.2, 0.2)         # Trailing edge ordinate
-    deltaYTE = (0, 0.1)       # Trailing edge thickness
-    alphaTE = (0, 360)        # Trailing edge direction angle
-    betaTE = (0, 360)         # Trailing edge wedge angle
+    Returns:
+    - Tuple[float, float]: The valid range for the parameter.
+    """
+    return parameter.valid_range
 
+def get_all_valid_ranges() -> Dict[str, Tuple[float, float]]:
+    """
+    Get a dictionary of all valid ranges for parameters.
 
+    Returns:
+    - Dict[str, Tuple[float, float]]: A dictionary with parameter names as keys and their valid ranges as values.
+    """
+    return {param.param_name: param.valid_range for param in Parameter}
 
-# # To Use
+# # Test usage
+# print("Valid range for rLE:", get_valid_range(Parameter.rLE))
+# print("All valid ranges:", get_all_valid_ranges())
 
-# from constraints import Constraint, Parameter
+# valid_ranges = {
+#         'rLE': (0.0085, 0.0126),
+#         'Xup': (0.41, 0.46),
+#         'Yup': (0.11, 0.13),
+#         'YXXup': (-0.9, -0.7),
+#         'Xlow': (0.20, 0.26),
+#         'Ylow': (-0.023, -0.015),
+#         'YXXlow': (0.05, 0.20),
+#         'yTE': (-0.006, -0.003),
+#         'deltaYTE': (0.0025, 0.0050),
+#         'alphaTE': (7.0, 10.0),
+#         'betaTE': (10.0, 14.0)
+#     }
 
-
-# chord_length = Constraint.CHORD_LENGTH.value
-# min_curvature = Constraint.MIN_CURVATURE.value
-# max_curvature = Constraint.MAX_CURVATURE.value
-# max_thickness = Constraint.MAX_THICKNESS.value
-
-
-# for param in Parameter:
-#     print(f"Parameter: {param.name}")
-
-# for constraint in Constraint:
-#     print(f"Constraint: {constraint.name}, Value: {constraint.value}")
-
-# print(f"Chord length: {Constraint.CHORD_LENGTH.value}")
+# print(valid_ranges == get_all_valid_ranges())
 
 
-# print(f"rLE min: {Parameter.rLE.min_value}, max: {Parameter.rLE.max_value}")
-# print(f"Xup soft min: {Parameter.Xup.soft_min}, soft max: {Parameter.Xup.soft_max}")
+
+
+
+
+
+
+
+
+
+
+
+# # Old Code
+
+# from enum import Enum, auto
+
+# # Option 1
+# class Parameter(Enum):
+#     rLE = auto()       # Leading edge radius
+#     Xup = auto()       # Upper crest abscissa
+#     Yup = auto()       # Upper crest ordinate
+#     YXXup = auto()     # Upper crest curvature
+#     Xlow = auto()      # Lower crest abscissa
+#     Ylow = auto()      # Lower crest ordinate
+#     YXXlow = auto()    # Lower crest curvature
+#     yTE = auto()       # Trailing edge ordinate
+#     deltaYTE = auto()  # Trailing edge thickness
+#     alphaTE = auto()   # Trailing edge direction angle
+#     betaTE = auto()    # Trailing edge wedge angle
+
+# class Constraint(Enum):
+#     CHORD_LENGTH = 1.0
+#     MIN_CURVATURE = -0.1
+#     MAX_CURVATURE = 0.1
+#     MAX_THICKNESS = 0.1
+
+
+# # Option 2 - with Flexibility
+# class Parameter(Enum):
+#     def __new__(cls, min_value, max_value, soft_min=None, soft_max=None):
+#         obj = object.__new__(cls)
+#         obj._value_ = obj  # Unique value for each member
+#         obj.min_value = min_value
+#         obj.max_value = max_value
+#         obj.soft_min = soft_min if soft_min is not None else min_value
+#         obj.soft_max = soft_max if soft_max is not None else max_value
+#         return obj
+
+#     rLE = (0.005, 0.05)       # Leading edge radius
+#     Xup = (0, 1.0)            # Upper crest abscissa
+#     Yup = (-0.2, 0.2)         # Upper crest ordinate
+#     YXXup = (-0.1, 0.1)       # Upper crest curvature
+#     Xlow = (0, 1.0)           # Lower crest abscissa
+#     Ylow = (-0.2, 0.2)        # Lower crest ordinate
+#     YXXlow = (-0.1, 0.1)      # Lower crest curvature
+#     yTE = (-0.2, 0.2)         # Trailing edge ordinate
+#     deltaYTE = (0, 0.1)       # Trailing edge thickness
+#     alphaTE = (0, 360)        # Trailing edge direction angle
+#     betaTE = (0, 360)         # Trailing edge wedge angle
+
+
+
+# # # To Use
+
+# # from constraints import Constraint, Parameter
+
+
+# # chord_length = Constraint.CHORD_LENGTH.value
+# # min_curvature = Constraint.MIN_CURVATURE.value
+# # max_curvature = Constraint.MAX_CURVATURE.value
+# # max_thickness = Constraint.MAX_THICKNESS.value
+
+
+# # for param in Parameter:
+# #     print(f"Parameter: {param.name}")
+
+# # for constraint in Constraint:
+# #     print(f"Constraint: {constraint.name}, Value: {constraint.value}")
+
+# # print(f"Chord length: {Constraint.CHORD_LENGTH.value}")
+
+
+# # print(f"rLE min: {Parameter.rLE.min_value}, max: {Parameter.rLE.max_value}")
+# # print(f"Xup soft min: {Parameter.Xup.soft_min}, soft max: {Parameter.Xup.soft_max}")
--- mutation.py
+++ mutation.py
@@ -312,3 +312,288 @@ def uniform_mutation(individual, chord_length, min_curvature, max_curvature, max
 #    *Justification:* This strategy is useful for a broad search of the solution space and can introduce significant diversity into the population. It is less biased than other methods and can be particularly effective in the early stages of the optimization process to ensure a good coverage of the search space.
 
 ##########################################################################
+
+
+# ## Old Code
+
+# import random
+
+# def random_resetting_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, mutation_rate):
+#     """
+#     Perform random resetting mutation on an individual.
+    
+#     Args:
+#     - individual (list): The individual genotype to mutate.
+#     - chord_length (float): The chord length of the airfoil.
+#     - min_curvature (float): The minimum allowable curvature.
+#     - max_curvature (float): The maximum allowable curvature.
+#     - max_thickness (float): The maximum allowable thickness as a proportion of chord length.
+#     - mutation_rate (float): The probability of mutation for each gene.
+    
+#     Returns:
+#     - mutated_individual (list): The mutated genotype.
+#     """
+#     # Define the ranges for each parameter based on the given constraints
+#     param_ranges = [
+#         (0.005 * chord_length, 0.05 * chord_length),  # rLE range
+#         (0, chord_length),                            # Xup range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Yup range
+#         (min_curvature, max_curvature),               # YXXup range
+#         (0, chord_length),                            # Xlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Ylow range
+#         (min_curvature, max_curvature),               # YXXlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # yTE range
+#         (0, max_thickness * chord_length),            # deltaYTE range
+#         (0, 360),                                     # alphaTE range
+#         (0, 360)                                      # betaTE range
+#     ]
+    
+#     # Perform mutation based on the mutation rate
+#     mutated_individual = individual[:]
+#     for i in range(len(mutated_individual)):
+#         if random.random() < mutation_rate:
+#             mutated_individual[i] = random.uniform(*param_ranges[i])
+    
+#     return mutated_individual
+
+# # # Test Random Resetting Mutation
+# # chord_length = 1.0  # Example chord length
+# # min_curvature = -0.1  # Example minimum curvature
+# # max_curvature = 0.1  # Example maximum curvature
+# # max_thickness = 0.1  # Example maximum thickness as a proportion of chord length
+# # mutation_rate = 0.1  # Example mutation rate (10%)
+
+# # individual = [0.03, 0.5, 0.1, 0.02, 0.4, -0.1, 0.01, 0.15, 0.005, 45, 30]
+# # mutated_individual = random_resetting_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, mutation_rate)
+
+# # print("Original Individual:", individual)
+# # print("Mutated Individual:", mutated_individual)
+
+
+
+# def creep_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, creep_rate, creep_magnitude):
+#     """
+#     Perform creep mutation on an individual.
+    
+#     Args:
+#     - individual (list): The individual genotype to mutate.
+#     - chord_length (float): The chord length of the airfoil.
+#     - min_curvature (float): The minimum allowable curvature.
+#     - max_curvature (float): The maximum allowable curvature.
+#     - max_thickness (float): The maximum allowable thickness as a proportion of chord length.
+#     - creep_rate (float): The probability of mutation for each gene.
+#     - creep_magnitude (float): The magnitude of the creep mutation.
+    
+#     Returns:
+#     - mutated_individual (list): The mutated genotype.
+#     """
+#     # Define the ranges for each parameter based on the given constraints
+#     param_ranges = [
+#         (0.005 * chord_length, 0.05 * chord_length),  # rLE range
+#         (0, chord_length),                            # Xup range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Yup range
+#         (min_curvature, max_curvature),               # YXXup range
+#         (0, chord_length),                            # Xlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Ylow range
+#         (min_curvature, max_curvature),               # YXXlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # yTE range
+#         (0, max_thickness * chord_length),            # deltaYTE range
+#         (0, 360),                                     # alphaTE range
+#         (0, 360)                                      # betaTE range
+#     ]
+    
+#     # Perform creep mutation based on the creep rate
+#     mutated_individual = individual[:]
+#     for i in range(len(mutated_individual)):
+#         if random.random() < creep_rate:
+#             # Apply creep mutation within the allowed range
+#             delta = random.uniform(-creep_magnitude, creep_magnitude)
+#             mutated_value = mutated_individual[i] + delta
+#             # Ensure the mutated value stays within bounds
+#             lower_bound, upper_bound = param_ranges[i]
+#             mutated_value = max(lower_bound, min(upper_bound, mutated_value))
+#             mutated_individual[i] = mutated_value
+    
+#     return mutated_individual
+
+# # # Test Creep Mutation
+# # chord_length = 1.0  # Example chord length
+# # min_curvature = -0.1  # Example minimum curvature
+# # max_curvature = 0.1  # Example maximum curvature
+# # max_thickness = 0.1  # Example maximum thickness as a proportion of chord length
+# # creep_rate = 0.1  # Example creep mutation rate (10%)
+# # creep_magnitude = 0.01  # Example creep magnitude
+
+# # individual = [0.03, 0.5, 0.1, 0.02, 0.4, -0.1, 0.01, 0.15, 0.005, 45, 30]
+# # mutated_individual = creep_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, creep_rate, creep_magnitude)
+
+# # print("Original Individual:", individual)
+# # print("Mutated Individual:", mutated_individual)
+
+
+
+# def clamp(value, min_value, max_value):
+#     """Clamp the value to be within the specified range."""
+#     return max(min_value, min(value, max_value))
+
+# def gaussian_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, mutation_rate, std_dev):
+#     """
+#     Perform Gaussian mutation on an individual.
+    
+#     Args:
+#     - individual (list): The individual genotype to mutate.
+#     - chord_length (float): The chord length of the airfoil.
+#     - min_curvature (float): The minimum allowable curvature.
+#     - max_curvature (float): The maximum allowable curvature.
+#     - max_thickness (float): The maximum allowable thickness as a proportion of chord length.
+#     - mutation_rate (float): The probability of mutation for each gene.
+#     - std_dev (float): The standard deviation for the Gaussian distribution.
+    
+#     Returns:
+#     - mutated_individual (list): The mutated genotype.
+#     """
+#     # Define the ranges for each parameter based on the given constraints
+#     param_ranges = [
+#         (0.005 * chord_length, 0.05 * chord_length),  # rLE range
+#         (0, chord_length),                            # Xup range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Yup range
+#         (min_curvature, max_curvature),               # YXXup range
+#         (0, chord_length),                            # Xlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Ylow range
+#         (min_curvature, max_curvature),               # YXXlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # yTE range
+#         (0, max_thickness * chord_length),            # deltaYTE range
+#         (0, 360),                                     # alphaTE range
+#         (0, 360)                                      # betaTE range
+#     ]
+    
+#     # Perform mutation based on the mutation rate
+#     mutated_individual = individual[:]
+#     for i in range(len(mutated_individual)):
+#         if random.random() < mutation_rate:
+#             mutation = random.gauss(0, std_dev)
+#             mutated_individual[i] += mutation
+#             # Ensure the mutated value is within the allowed range
+#             mutated_individual[i] = clamp(mutated_individual[i], *param_ranges[i])
+    
+#     return mutated_individual
+
+# # # Test Gaussian Mutation
+# # chord_length = 1.0  # Example chord length
+# # min_curvature = -0.1  # Example minimum curvature
+# # max_curvature = 0.1  # Example maximum curvature
+# # max_thickness = 0.1  # Example maximum thickness as a proportion of chord length
+# # mutation_rate = 0.1  # Example mutation rate (10%)
+# # std_dev = 0.01  # Example standard deviation for Gaussian distribution
+
+# # individual = [0.03, 0.5, 0.1, 0.02, 0.4, -0.1, 0.01, 0.15, 0.005, 45, 30]
+# # mutated_individual = gaussian_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, mutation_rate, std_dev)
+
+# # print("Original Individual:", individual)
+# # print("Mutated Individual:", mutated_individual)
+
+
+# def boundary_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, mutation_rate):
+#     """
+#     Perform boundary mutation on an individual.
+    
+#     Args:
+#     - individual (list): The individual genotype to mutate.
+#     - chord_length (float): The chord length of the airfoil.
+#     - min_curvature (float): The minimum allowable curvature.
+#     - max_curvature (float): The maximum allowable curvature.
+#     - max_thickness (float): The maximum allowable thickness as a proportion of chord length.
+#     - mutation_rate (float): The probability of mutation for each gene.
+    
+#     Returns:
+#     - mutated_individual (list): The mutated genotype.
+#     """
+#     # Define the ranges for each parameter based on the given constraints
+#     param_ranges = [
+#         (0.005 * chord_length, 0.05 * chord_length),  # rLE range
+#         (0, chord_length),                            # Xup range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Yup range
+#         (min_curvature, max_curvature),               # YXXup range
+#         (0, chord_length),                            # Xlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Ylow range
+#         (min_curvature, max_curvature),               # YXXlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # yTE range
+#         (0, max_thickness * chord_length),            # deltaYTE range
+#         (0, 360),                                     # alphaTE range
+#         (0, 360)                                      # betaTE range
+#     ]
+    
+#     # Perform mutation based on the mutation rate
+#     mutated_individual = individual[:]
+#     for i in range(len(mutated_individual)):
+#         if random.random() < mutation_rate:
+#             # Choose one of the boundaries at random for the mutation
+#             mutated_individual[i] = random.choice(param_ranges[i])
+    
+#     return mutated_individual
+
+# # # Test Boundary Mutation
+# # # We can use prior knowledge for better reuslts in boundaries
+# # chord_length = 1.0  # Example chord length
+# # min_curvature = -0.1  # Example minimum curvature
+# # max_curvature = 0.1  # Example maximum curvature
+# # max_thickness = 0.1  # Example maximum thickness as a proportion of chord length
+# # mutation_rate = 0.1  # Example mutation rate (10%)
+
+# # individual = [0.03, 0.5, 0.1, 0.02, 0.4, -0.1, 0.01, 0.15, 0.005, 45, 30]
+# # mutated_individual = boundary_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, mutation_rate)
+
+# # print("Original Individual:", individual)
+# # print("Mutated Individual:", mutated_individual)
+
+
+# def uniform_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, mutation_rate):
+#     """
+#     Perform uniform mutation on an individual.
+    
+#     Args:
+#     - individual (list): The individual genotype to mutate.
+#     - chord_length (float): The chord length of the airfoil.
+#     - min_curvature (float): The minimum allowable curvature.
+#     - max_curvature (float): The maximum allowable curvature.
+#     - max_thickness (float): The maximum allowable thickness as a proportion of chord length.
+#     - mutation_rate (float): The probability of mutation for each gene.
+    
+#     Returns:
+#     - mutated_individual (list): The mutated genotype.
+#     """
+#     # Define the ranges for each parameter based on the given constraints
+#     param_ranges = [
+#         (0.005 * chord_length, 0.05 * chord_length),  # rLE range
+#         (0, chord_length),                            # Xup range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Yup range
+#         (min_curvature, max_curvature),               # YXXup range
+#         (0, chord_length),                            # Xlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # Ylow range
+#         (min_curvature, max_curvature),               # YXXlow range
+#         (-0.2 * chord_length, 0.2 * chord_length),    # yTE range
+#         (0, max_thickness * chord_length),            # deltaYTE range
+#         (0, 360),                                     # alphaTE range
+#         (0, 360)                                      # betaTE range
+#     ]
+    
+#     # Perform mutation based on the mutation rate
+#     mutated_individual = individual[:]
+#     for i in range(len(mutated_individual)):
+#         if random.random() < mutation_rate:
+#             mutated_individual[i] = random.uniform(*param_ranges[i])
+    
+#     return mutated_individual
+
+# # # Test Uniform Mutation
+# # chord_length = 1.0  # Example chord length
+# # min_curvature = -0.1  # Example minimum curvature
+# # max_curvature = 0.1  # Example maximum curvature
+# # max_thickness = 0.1  # Example maximum thickness as a proportion of chord length
+# # mutation_rate = 0.1  # Example mutation rate (10%)
+
+# # individual = [0.03, 0.5, 0.1, 0.02, 0.4, -0.1, 0.01, 0.15, 0.005, 45, 30]
+# # mutated_individual = uniform_mutation(individual, chord_length, min_curvature, max_curvature, max_thickness, mutation_rate)
+
+# # print("Original Individual:", individual)
+# # print("Mutated Individual:", mutated_individual)
