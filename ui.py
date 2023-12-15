6e73d8e9f9b1c34d047ff34c7052005c6e710518
akarshani ramanayake <chethana.20200288@iit.ac.lk>
2023-12-15 22:23:02 +0530

    custom signal was added to emit a signal whenever a generation end.

--- ui.py
+++ ui.py
@@ -2,10 +2,13 @@ from PySide6.QtWidgets import QMainWindow, QLabel, QComboBox, QCheckBox, QLineEd
     QGridLayout, QPushButton
 from PySide6.QtCharts import QLineSeries, QChart, QChartView,QXYSeries
 from PySide6.QtGui import QPainter
-from PySide6.QtCore import QPointF, Slot
+from PySide6.QtCore import QPointF, Slot,QObject, Signal
 import pyqtgraph as pg
 
+import re as regex
+import os
 from random import randint
+
 from tqdm import tqdm
 
 # AIRFOIL
@@ -16,36 +19,44 @@ PointConfig = QXYSeries.PointConfiguration
 import optimization
 
 class ChartWindow(QMainWindow):
+    updateGeneration = Signal()
+
+    # initial airfoil
+    rLE, Xup, Yup, YXXup, Xlow, Ylow, YXXlow, yTE, deltaYTE, alphaTE, betaTE = 0.0147, 0.3015, 0.0599, -0.4360, 0.2996, -0.06, 0.4406, 0, 0, 0, 14.67
+    airfoil = airfoil_Builder.Airfoil_Builder(rLE, Xup, Yup, YXXup, Xlow, Ylow, YXXlow, yTE, deltaYTE, alphaTE, betaTE)
+    airfoil.build()
+    xcoor = airfoil.XCoordinates
+    yCoorUpper = airfoil.YCoordinatesUpper
+    yCoorLower = airfoil.YCoordinatesLower
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Parsec Airfoil")
 
-        # lakindu - code starts here
-        ######################
+        # initialize parameters
+        xcoor = self.xcoor
+        yCoorUpper = self.yCoorUpper
+        yCoorLower = self.yCoorLower
+        self.gen_number = [0]
+        self.highest_fitness = [0]
 
-        rLE, Xup, Yup, YXXup, Xlow, Ylow, YXXlow, yTE, deltaYTE, alphaTE, betaTE = 0.0147,0.3015,0.0599,-0.4360,0.2996,-0.06,0.4406,0,0,0,14.67
-        airfoil = airfoil_Builder.Airfoil_Builder(rLE, Xup, Yup, YXXup, Xlow, Ylow, YXXlow, yTE, deltaYTE, alphaTE,betaTE)
-        airfoil.build()
-        xcoor = airfoil.XCoordinates
-        yCoorUpper = airfoil.YCoordinatesUpper
-        yCoorLower = airfoil.YCoordinatesLower
+        self.fitness = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+        self.aoa = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
 
-        self.graphWidget = pg.PlotWidget()
-        self.x = list(range(100))  # 100 time points
-        self.y = [randint(0, 100) for _ in range(100)]  # 100 data points
-        self.graphWidget.setBackground('w')
+        self._fitness_graph_Widget = pg.PlotWidget()
+        self._fitness_graph_Widget.setBackground('w')
         pen = pg.mkPen(color=(255, 0, 0))
-        self.data_line = self.graphWidget.plot(self.x, self.y, pen=pen)
-        self.timer = pg.QtCore.QTimer()
-        self.timer.setInterval(500)
-        self.timer.timeout.connect(self.update_plot_data)
-        self.timer.start()
+        self.data_line = self._fitness_graph_Widget.plot(self.gen_number, self.highest_fitness, pen=pen)
+
+        self._expanded_fitness_graph_Widget = pg.PlotWidget()
+        self._expanded_fitness_graph_Widget.setBackground('w')
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.expanded_fitness_data_line1 = self._expanded_fitness_graph_Widget.plot(self.aoa, self.fitness[0], pen=pen)
 
         # Labels
         fitness_parameters_label = QLabel("fitness parameters")
         reynold_label = QLabel("Reynold Number: ")
         mach_label = QLabel("Mach Number: ")
-        label_visibility_label = QLabel("Label Visibility: ")
 
         rLE_label = QLabel("rLE: ")
         Xup_label = QLabel("Xup: ")
@@ -63,61 +74,23 @@ class ChartWindow(QMainWindow):
         self._reynold_combobox = QComboBox()
         self._mach_combobox = QComboBox()
         self.optimization_button = QPushButton("Optimize")
-        #self.optimization_button.move(100, 100)
 
-        self._label_visibility_checkbox = QCheckBox()
-
-        self._rLE_lineedit = QLineEdit(str(rLE))
-        self._Xup_lineedit = QLineEdit(str(Xup))
-        self._Yup_lineedit = QLineEdit(str(Yup))
-        self._YXXup_lineedit = QLineEdit(str(YXXup))
-        self._Xlow_lineedit = QLineEdit(str(Xlow))
-        self._Ylow_lineedit = QLineEdit(str(Ylow))
-        self._YXXlow_lineedit = QLineEdit(str(YXXlow))
-        self._yTE_lineedit = QLineEdit(str(yTE))
-        self._deltaYTE_lineedit = QLineEdit(str(deltaYTE))
-        self._alphaTE_lineedit = QLineEdit(str(alphaTE))
-        self._betaTE_lineedit = QLineEdit(str(betaTE))
+        self._rLE_lineedit = QLineEdit(str(self.rLE))
+        self._Xup_lineedit = QLineEdit(str(self.Xup))
+        self._Yup_lineedit = QLineEdit(str(self.Yup))
+        self._YXXup_lineedit = QLineEdit(str(self.YXXup))
+        self._Xlow_lineedit = QLineEdit(str(self.Xlow))
+        self._Ylow_lineedit = QLineEdit(str(self.Ylow))
+        self._YXXlow_lineedit = QLineEdit(str(self.YXXlow))
+        self._yTE_lineedit = QLineEdit(str(self.yTE))
+        self._deltaYTE_lineedit = QLineEdit(str(self.deltaYTE))
+        self._alphaTE_lineedit = QLineEdit(str(self.alphaTE))
+        self._betaTE_lineedit = QLineEdit(str(self.betaTE))
 
         self._chart = QChart()
-
         self._series = QLineSeries()
         self._series.setName("Airfoil")
         self._series.setPointsVisible(True)
-        self._series.append([QPointF(0,0),
-                             QPointF(xcoor[0],yCoorUpper[0]),
-                             QPointF(xcoor[1],yCoorUpper[1]),
-                             QPointF(xcoor[2],yCoorUpper[2]),
-                             QPointF(xcoor[3],yCoorUpper[3]),
-                             QPointF(xcoor[4],yCoorUpper[4]),
-                             QPointF(xcoor[5],yCoorUpper[5]),
-                             QPointF(xcoor[6],yCoorUpper[6]),
-                             QPointF(xcoor[7],yCoorUpper[7]),
-                             QPointF(xcoor[8],yCoorUpper[8]),
-                             QPointF(xcoor[9],yCoorUpper[9]),
-                             QPointF(xcoor[10], yCoorUpper[10]),
-                             QPointF(xcoor[11], yCoorUpper[11]),
-                             QPointF(xcoor[12], yCoorUpper[12]),
-                             QPointF(xcoor[13], yCoorUpper[13]),
-                             QPointF(xcoor[14], yCoorUpper[14]),
-
-                             QPointF(xcoor[0], yCoorLower[0]),
-                             QPointF(xcoor[1], yCoorLower[1]),
-                             QPointF(xcoor[2], yCoorLower[2]),
-                             QPointF(xcoor[3], yCoorLower[3]),
-                             QPointF(xcoor[4], yCoorLower[4]),
-                             QPointF(xcoor[5], yCoorLower[5]),
-                             QPointF(xcoor[6], yCoorLower[6]),
-                             QPointF(xcoor[7], yCoorLower[7]),
-                             QPointF(xcoor[8], yCoorLower[8]),
-                             QPointF(xcoor[9], yCoorLower[9]),
-                             QPointF(xcoor[10], yCoorLower[10]),
-                             QPointF(xcoor[11], yCoorLower[11]),
-                             QPointF(xcoor[12], yCoorLower[12]),
-                             QPointF(xcoor[13], yCoorLower[13]),
-                             QPointF(xcoor[14], yCoorLower[14])
-                             ])
-
         self._chart.addSeries(self._series)
         self._chart.createDefaultAxes()
 
@@ -144,6 +117,9 @@ class ChartWindow(QMainWindow):
         self._betaTE_lineedit.editingFinished.connect(self._set_custom_label)
 
         self.optimization_button.clicked.connect(self._run_optimizer)
+        self.updateGeneration.connect(self._update_generation)
+
+        # layout
         chart_view = QChartView(self._chart)
         chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
 
@@ -188,19 +164,12 @@ class ChartWindow(QMainWindow):
 
         genotype_layout.addWidget(self.optimization_button,12,1)
 
-        # label_checkbox = self._label_checkbox
-        # label_checkbox.clicked.connect(self._set_label_visibility)
-        # control_layout.addWidget(selected_point_index_label, 0, 0)
-        # control_layout.addWidget(self._selected_point_index_lineedit, 0, 1)
-
-        control_layout.addWidget(label_visibility_label, 3, 0)
-        control_layout.addWidget(self._label_visibility_checkbox, 3, 1, 1, 2)
-
         main_widget = QWidget(self)
         main_layout = QHBoxLayout(main_widget)
         main_layout.addWidget(chart_view)
         main_layout.addWidget(control_widget)
-        control_layout.addWidget(self.graphWidget)
+        control_layout.addWidget(self._fitness_graph_Widget)
+        main_layout.addWidget(self._expanded_fitness_graph_Widget)
         main_layout.setStretch(0, 1)
         self.setCentralWidget(main_widget)
 
@@ -212,40 +181,141 @@ class ChartWindow(QMainWindow):
     def _set_mach(self, index: int):
             print("index")
 
-    @Slot(bool)
-    def _set_label_visibility(self, checked: bool):
-        print("")
-
     @Slot()
     def _set_custom_label(self):
         print("...value changed")
 
     @Slot()
     def _run_optimizer(self):
-
         # Run optimization_strategy_one for 100 Generations
-
         # Generate initial population
         initial_population = optimization.generate_population(10)
 
         # Run optimization_strategy_one for 100 generations
         current_generation = initial_population
-        for i in tqdm(list(range(2))):
+        self.updateGeneration.emit()
+        for i in tqdm(list(range(3))):
             current_generation = optimization.optimization_strategy_one(current_generation, 10)
             optimization.log_genration_results(current_generation, i + 1)
+            self.updateGeneration.emit()
 
         # Evaluate fitness of final generation
         fitness_scores = optimization.lift_coef_based_fitness_function_multi(current_generation)
         return current_generation, fitness_scores
 
-    def update_plot_data(self):
+    @Slot()
+    def _update_generation(self):
+        folder_path = "/Users/ak/Desktop/4th year/computational intelligence/project/CI-CW-Airfoil-Optimization/RESULTS/Experiment1"
+        files = os.listdir(folder_path)
+        text_files = [file for file in files if file.endswith(".txt")]
 
-        self.x = self.x[1:]  # Remove the first y element.
-        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+        max_timestamp_file = ""
+        if text_files:
+            max_timestamp_file = max(text_files, key=self.extract_timestamp)
 
-        self.y = self.y[1:]  # Remove the first
-        self.y.append(randint(0, 100))  # Add a new random value.
+        full_path = os.path.join(folder_path, max_timestamp_file)
 
-        self.data_line.setData(self.x, self.y)  # Update the data.
+        with open(full_path, 'r') as file:
+            content = file.read()
+            # Extract generation number
+            generation_match = regex.search(r"Generation: (\d+)", content)
+            generation_number = int(generation_match.group(1)) if generation_match else None
+
+            # Extract the first array in Genotypes and their Fitness Values
+            genotype_match = regex.search(r"\[([^\]]+)] - Fitness:", content)
+            genotype_array = [float(x) for x in genotype_match.group(1).split(",")] if genotype_match else None
+
+        rLE, Xup, Yup, YXXup, Xlow, Ylow, YXXlow, yTE, deltaYTE, alphaTE, betaTE = \
+            genotype_array[0], genotype_array[1], genotype_array[2], genotype_array[3], genotype_array[4], \
+                genotype_array[5], \
+                genotype_array[6], genotype_array[7], genotype_array[8], genotype_array[9], genotype_array[10]
+        airfoil = airfoil_Builder.Airfoil_Builder(rLE, Xup, Yup, YXXup, Xlow, Ylow, YXXlow, yTE, deltaYTE, alphaTE,
+                                                  betaTE)
+        airfoil.build()
+        xcoor = airfoil.XCoordinates
+        yCoorUpper = airfoil.YCoordinatesUpper
+        yCoorLower = airfoil.YCoordinatesLower
+
+        self._rLE_lineedit.setText(str(rLE))
+        self._Xup_lineedit.setText(str(Xup))
+        self._Yup_lineedit.setText(str(Yup))
+        self._YXXup_lineedit.setText(str(YXXup))
+        self._Xlow_lineedit.setText(str(Xlow))
+        self._Ylow_lineedit.setText(str(Ylow))
+        self._YXXlow_lineedit.setText(str(YXXlow))
+        self._yTE_lineedit.setText(str(yTE))
+        self._deltaYTE_lineedit.setText(str(deltaYTE))
+        self._alphaTE_lineedit.setText(str(alphaTE))
+        self._betaTE_lineedit.setText(str(betaTE))
+
+        self._series = QLineSeries()
+        self._series.append([QPointF(0, 0),
+                             QPointF(xcoor[0], yCoorUpper[0]),
+                             QPointF(xcoor[1], yCoorUpper[1]),
+                             QPointF(xcoor[2], yCoorUpper[2]),
+                             QPointF(xcoor[3], yCoorUpper[3]),
+                             QPointF(xcoor[4], yCoorUpper[4]),
+                             QPointF(xcoor[5],yCoorUpper[5]),
+                             QPointF(xcoor[6],yCoorUpper[6]),
+                             QPointF(xcoor[7],yCoorUpper[7]),
+                             QPointF(xcoor[8],yCoorUpper[8]),
+                             QPointF(xcoor[9],yCoorUpper[9]),
+                             QPointF(xcoor[10], yCoorUpper[10]),
+                             QPointF(xcoor[11], yCoorUpper[11]),
+                             QPointF(xcoor[12], yCoorUpper[12]),
+                             QPointF(xcoor[13], yCoorUpper[13]),
+                             QPointF(xcoor[14], yCoorUpper[14]),
+                             QPointF(1, 0),
+
+                             QPointF(xcoor[14], yCoorLower[14]),
+                             QPointF(xcoor[13], yCoorLower[13]),
+                             QPointF(xcoor[12], yCoorLower[12]),
+                             QPointF(xcoor[11], yCoorLower[11]),
+                             QPointF(xcoor[10], yCoorLower[10]),
+                             QPointF(xcoor[9], yCoorLower[9]),
+                             QPointF(xcoor[8], yCoorLower[8]),
+                             QPointF(xcoor[7], yCoorLower[7]),
+                             QPointF(xcoor[6], yCoorLower[6]),
+                             QPointF(xcoor[5], yCoorLower[5]),
+                             QPointF(xcoor[4], yCoorLower[4]),
+                             QPointF(xcoor[3], yCoorLower[3]),
+                             QPointF(xcoor[2], yCoorLower[2]),
+                             QPointF(xcoor[1], yCoorLower[1]),
+                             QPointF(xcoor[0], yCoorLower[0]),
+                             QPointF(0, 0)])
+
+        highest_Fitness = 1
+        gen_number = 1
+        f = [1.9,2.3,2,2.5,2.9,3,3.3,3,5.5,5,6,6.7,6.8,5,4,4.9,4.5,3,3.9,1,1.5]
+        self.fitness.append(f)
+
+        self._chart.removeAllSeries()
+        self._chart.addSeries(self._series)
+
+        self.gen_number.append(self.gen_number[-1]+gen_number)
+        print(self.gen_number)
+        self.highest_fitness.append(self.highest_fitness[-1]+highest_Fitness)
+        print(self.highest_fitness)
+        self.data_line.setData(self.highest_fitness, self.highest_fitness)  # Update the data.
+        for i in range(len(self.fitness)):
+            pen = pg.mkPen(color=(255, 0, 0))
+            self.expanded_fitness_data_line2 = self._expanded_fitness_graph_Widget.plot( self.aoa,self.fitness[i] , pen=pen)
+
+        print("a new generation started.")
+
+    def extract_timestamp(self, file_name):
+        time_stamp = file_name.split("_")[-1].split(".")[0]
+        return int(time_stamp)
+
+    #
+    # def update_plot_data(self):
+    #     self.x = self.x[1:]  # Remove the first y element.
+    #     self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+    #
+    #     self.y = self.y[1:]  # Remove the first
+    #     self.y.append(randint(0, 100))  # Add a new random value.
+    #
+    #     self.data_line.setData(self.x, self.y)  # Update the data.
+
 
 
